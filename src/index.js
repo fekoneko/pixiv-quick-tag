ddef403d002a01c58256a0386e83509842f02922
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-02-16 17:42:19 +0300

    feat: tag inputs logic

--- src/index.js
+++ src/index.js
@@ -3,42 +3,67 @@ if (!root) throw new Error('Pixiv Quick Tag could not find #root element');
 
 let rootObserver;
 
-const addTagButtons = () => {
+const appendWorkMenuInputSection = (workMenu) => {
+  const inputSection = document.createElement('div');
+  inputSection.classList.add('pixiv-quick-tag-input-section');
+  workMenu.appendChild(inputSection);
+
+  const appendInput = () => {
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.name = 'tags';
+    input.placeholder = 'Add tag...';
+    input.oninput = () => {
+      if (input.value.length > 0 && inputSection.lastChild?.value !== '') {
+        appendInput();
+      }
+      if (input.value.length === 0 && inputSection.childNodes.length > 1) {
+        input.remove();
+        inputSection.lastChild?.focus();
+      }
+    };
+    inputSection.appendChild(input);
+  };
+  appendInput();
+};
+
+const appendWorkMenuApplySection = (workMenu) => {
+  const applySection = document.createElement('div');
+  applySection.classList.add('pixiv-quick-tag-apply-section');
+  workMenu.appendChild(applySection);
+
+  const applyButton = document.createElement('button');
+  applyButton.type = 'submit';
+  applyButton.textContent = 'Apply';
+  applySection.appendChild(applyButton);
+
+  const privateCheckbox = document.createElement('input');
+  privateCheckbox.type = 'checkbox';
+  privateCheckbox.id = 'pixiv-quick-tag-private-checkbox';
+  applySection.appendChild(privateCheckbox);
+
+  const privateLabel = document.createElement('label');
+  privateLabel.textContent = 'private';
+  privateLabel.htmlFor = privateCheckbox.id;
+  applySection.appendChild(privateLabel);
+};
+
+const appendWorkMenus = () => {
   const workLinks = document.querySelectorAll('a[data-gtm-value][href^="/artworks/"]');
   workLinks.forEach((workLink) => {
     const workId = workLink.href.split('/').pop();
     const workContainer = workLink.parentElement.parentElement.parentElement;
 
-    const tagButtonsContainer = document.createElement('div');
-    workContainer.appendChild(tagButtonsContainer);
-    tagButtonsContainer.classList.add('pixiv-quick-tag-buttons');
+    const workMenu = document.createElement('form');
+    workContainer.appendChild(workMenu);
+    workMenu.classList.add('pixiv-quick-tag-work-menu');
 
-    const tagInput = document.createElement('input');
-    tagInput.type = 'text';
-    tagInput.placeholder = 'Add tag...';
-    tagButtonsContainer.appendChild(tagInput);
-
-    const applySection = document.createElement('div');
-    applySection.classList.add('apply-section');
-    tagButtonsContainer.appendChild(applySection);
-
-    const applyButton = document.createElement('button');
-    applyButton.textContent = 'Apply';
-    applySection.appendChild(applyButton);
-
-    const privateCheckbox = document.createElement('input');
-    privateCheckbox.type = 'checkbox';
-    privateCheckbox.id = 'pixiv-quick-tag-private-checkbox';
-    applySection.appendChild(privateCheckbox);
-
-    const privateLabel = document.createElement('label');
-    privateLabel.textContent = 'private';
-    privateLabel.htmlFor = privateCheckbox.id;
-    applySection.appendChild(privateLabel);
+    appendWorkMenuInputSection(workMenu);
+    appendWorkMenuApplySection(workMenu);
 
     rootObserver.disconnect();
   });
 };
 
-rootObserver = new MutationObserver(addTagButtons);
+rootObserver = new MutationObserver(appendWorkMenus);
 rootObserver.observe(root, { childList: true, subtree: true });
